ce2330f0f9dfdd40a4adbf63d010040555d09ea7
Ryan Corlew <ryancorlew13@gmail.com>
2020-06-13 08:56:28 -0400

    Updates

---
+++ precache-manifest.5e6df88b2890e486fe59f422f615ec1d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d4288ad4eeeccc0cb3e8658a64f74f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "295ae9f5d1a112dd0b5f",
+    "url": "/portfolio/static/css/main.c4e4b21d.chunk.css"
+  },
+  {
+    "revision": "238aeaaded0cff5293df",
+    "url": "/portfolio/static/js/2.a2f951ac.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/portfolio/static/js/2.a2f951ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295ae9f5d1a112dd0b5f",
+    "url": "/portfolio/static/js/main.fcab7f26.chunk.js"
+  },
+  {
+    "revision": "039eb055909a0ac81ca5",
+    "url": "/portfolio/static/js/runtime-main.3e7fea94.js"
+  },
+  {
+    "revision": "da209dc3444a4c49ca0af5419b548e79",
+    "url": "/portfolio/static/media/destiny.da209dc3.png"
+  },
+  {
+    "revision": "66e2ae46c0347b40c76d6fed1c21b1e8",
+    "url": "/portfolio/static/media/food.66e2ae46.png"
+  },
+  {
+    "revision": "e117f0af577f77cda8cfb148fd590598",
+    "url": "/portfolio/static/media/me.e117f0af.JPG"
+  },
+  {
+    "revision": "92c48697977ed4447908f19843ea05f2",
+    "url": "/portfolio/static/media/recipe.92c48697.png"
+  },
+  {
+    "revision": "ea661a015cc52a3c78be28d6b2df1586",
+    "url": "/portfolio/static/media/todo.ea661a01.png"
+  }
+]);
